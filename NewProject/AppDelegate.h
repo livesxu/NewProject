8869c4bb7913c4859428ff59bc6a9892c6f3ecaf
livesxu <livesxu@163.com>
2016-10-10 15:14:58 +0800

    Initial Commit

---
+++ NewProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewProject
+//
+//  Created by Livespro on 16/10/10.
+//  Copyright © 2016年 FZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NewProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NewProject
+//
+//  Created by Livespro on 16/10/10.
+//  Copyright © 2016年 FZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
